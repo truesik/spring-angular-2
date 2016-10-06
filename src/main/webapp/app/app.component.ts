4015b211ae9c9eb3cd8c7dd332aff9c009a120f6
truesik <truesik@gmail.com>
2016-10-06 18:39:56 +0300

    moved

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular App</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+}
 
